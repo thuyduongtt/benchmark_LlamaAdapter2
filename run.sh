04e82c8e20a4b68a6fb5358d5c1ed0881ac4a1f1
Duong Tran <d@dttt.io>
2024-03-03 17:24:53 +0100

    1st commit

--- run.sh
+++ run.sh
@@ -2,13 +2,11 @@
 
 case $1 in
   1)
-    TASK='vqa'
     DS_NAME="unbalanced"
     MODEL_NAME="LORA-BIAS-7B-v21"
     LLAMA_TYPE="7B"
     ;;
   2)
-    TASK='vqa'
     DS_NAME="balanced_10"
     MODEL_NAME="LORA-BIAS-7B-v21"
     LLAMA_TYPE="7B"
@@ -17,14 +15,12 @@ esac
 
 DS_DIR="../dataset/${DS_NAME}"
 python start.py \
- --task $TASK \
  --path_to_ds $DS_DIR \
  --output_dir_name output_${MODEL_NAME}_${LLAMA_TYPE}_${DS_NAME} \
  --model_name $MODEL_NAME \
  --llama_type $LLAMA_TYPE
 
 python start.py \
- --task $TASK \
  --path_to_ds $DS_DIR \
  --output_dir_name output_${MODEL_NAME}_${LLAMA_TYPE}_${DS_NAME}_test \
  --split test \
--- start.py
+++ start.py
@@ -5,7 +5,7 @@ from PIL import Image
 import cv2
 import llama
 
-from pipeline import run_pipeline_by_question, run_pipeline_by_image
+from pipeline import run_pipeline_by_question
 
 llama_model = None
 MODEL_NAME = ''
@@ -25,17 +25,15 @@ def vqa_task(image, row_data):
 
     if llama_model is None:
         load_model()
+
     model, preprocess = llama_model
+    model.eval()
 
     img = Image.fromarray(cv2.imread(image))
     img = preprocess(img).unsqueeze(0).to(device)
 
     prompt = llama.format_prompt(row_data['question'])
     return model.generate(img, [prompt])[0]
-
-
-def image_captioning_task(image):
-    return ''
 
 
 def main():
@@ -47,22 +45,14 @@ def main():
     parser.add_argument('--limit', type=int, default=0, help='Max number of samples')
     parser.add_argument('--model_name', type=str, default='LORA-BIAS-7B-v21')
     parser.add_argument('--llama_type', type=str, default='7B')
-    parser.add_argument('--task', type=str, default='vqa', help='Task name: vqa, image_captioning')
     args = parser.parse_args()
 
     global MODEL_NAME, LLAMA_TYPE
     MODEL_NAME = args.model_name
     LLAMA_TYPE = args.llama_type
 
-    if args.task == 'vqa':
-        run_pipeline_by_question(vqa_task, args.path_to_ds, args.output_dir_name, limit=args.limit,
-                                 start_at=args.start_at, split=args.split)
-    elif args.task == 'image_captioning':
-        run_pipeline_by_image(image_captioning_task, args.path_to_ds, args.output_dir_name, limit=args.limit,
-                              start_at=args.start_at, split=args.split)
-
-    else:
-        print('Invalid task')
+    run_pipeline_by_question(vqa_task, args.path_to_ds, args.output_dir_name, limit=args.limit,
+                             start_at=args.start_at, split=args.split)
 
 
 if __name__ == '__main__':
